b9326b02f6980c759cd07d7f15d995875dcd8aef
KhangPBM <phamkhang12378@gmail.com>
2024-05-17 19:06:07 +0700

    update: add service registration

---
+++ TravelCapstone.BackEnd.API/Controllers/EventController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TravelCapstone.BackEnd.Application.IServices;
+using TravelCapstone.BackEnd.Common.DTO.Request;
+using TravelCapstone.BackEnd.Common.DTO.Response;
+
+namespace TravelCapstone.BackEnd.API.Controllers
+{
+    [Route("event")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private IEventService _service;
+        public EventController(IEventService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("get-event-list-with-quantity")]
+        public async Task<AppActionResult> GetEventListWithQuantity(int quantity)
+        {
+            return await _service.GetEventListWithQuantity(quantity);
+        }
+    }
+}
