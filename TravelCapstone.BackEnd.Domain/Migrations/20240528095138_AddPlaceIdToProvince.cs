f585c88a7a01dfb36a863db9380645a77ba07a6a
KhangPBM <phamkhang12378@gmail.com>
2024-05-28 21:02:58 +0700

    optimize get Optimal Path

---
+++ TravelCapstone.BackEnd.Domain/Migrations/20240528095138_AddPlaceIdToProvince.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelCapstone.BackEnd.Domain.Migrations
+{
+    public partial class AddPlaceIdToProvince : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GoongPlaceId",
+                table: "Provinces",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GoongPlaceId",
+                table: "Provinces");
+        }
+    }
+}
