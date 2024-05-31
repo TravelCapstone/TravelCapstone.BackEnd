bc45a20a146526df8f57c5f7de7f14943f1471f1
KhangPBM <phamkhang12378@gmail.com>
2024-05-31 23:45:07 +0700

    update

---
+++ TravelCapstone.BackEnd.Common/DTO/Response/RoomSuggestionResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelCapstone.BackEnd.Common.DTO.Response
+{
+    public class RoomSuggestionResponse
+    {
+        public int RoomSize { get; set; }
+        public int NumOfRoom { get; set; }
+
+    }
+}
