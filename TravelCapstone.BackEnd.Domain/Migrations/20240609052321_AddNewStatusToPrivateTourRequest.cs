3a233e371de93f08b9613dad0f4e704595f0b2f5
KhangPBM <phamkhang12378@gmail.com>
2024-06-09 12:36:41 +0700

    update

---
+++ TravelCapstone.BackEnd.Domain/Migrations/20240609052321_AddNewStatusToPrivateTourRequest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelCapstone.BackEnd.Domain.Migrations
+{
+    public partial class AddNewStatusToPrivateTourRequest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "PrivateTourStatuses",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 4, "PLANCREATED" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PrivateTourStatuses",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
