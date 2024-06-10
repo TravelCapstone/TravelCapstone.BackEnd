31a370a11e21ea1c4855d678aeadad059d36230c
Hà Minh Quân <97644076+whitedolpin@users.noreply.github.com>
2024-06-10 21:08:07 +0700

    commit code

--- TravelCapstone.BackEnd.Application/IServices/ITourService.cs
+++ TravelCapstone.BackEnd.Application/IServices/ITourService.cs
@@ -1,5 +1,6 @@
 using TravelCapstone.BackEnd.Common.DTO.Request;
 using TravelCapstone.BackEnd.Common.DTO.Response;
+using TravelCapstone.BackEnd.Domain.Enum;
 
 namespace TravelCapstone.BackEnd.Application.IServices;
 
@@ -11,5 +12,5 @@ public interface ITourService
     Task<AppActionResult> RegisterTour(TourRegistrationDto dto);
     Task<AppActionResult> CreateTour(CreatePlanDetailDto dto);
     Task<AppActionResult> GetPlanByTour(Guid tourId);
-
+    Task<AppActionResult> UpdateOptionStatus(Guid optionId);    
 }
